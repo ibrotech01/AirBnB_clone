8f0c97964c6d508546fdf295a88c7321fd074cd8
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-05-07 22:59:45 +0100

    update the database engine

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -70,4 +70,4 @@ class DBStorage:
     def close(self):
         """ call remove() method on the private session attribute
             (self.__session) or close() on the class Session """
-        self.__session.remove()
+        self.__session.close()
