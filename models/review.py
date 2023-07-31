59d7574f16b80434419ea4e7078bf85f041ef8ad
ibrotech01 <45831834+ibrotech01@users.noreply.github.com>
2023-07-31 01:36:16 +0100

    review.py

--- models/review.py
+++ models/review.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python3
-"""This is the review class"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+"""Defines the Place class."""
+from models.base_model import BaseModel
 
 
-class Review(BaseModel, Base):
-    """This is the class for Review
+class Place(BaseModel):
+    """Represent a place.
     Attributes:
-        place_id: place id
-        user_id: user id
-        text: review description
+        city_id (str): The City id.
+        user_id (str): The User id.
+        name (str): The name of the place.
+        description (str): The description of the place.
+        number_rooms (int): The number of rooms of the place.
+        number_bathrooms (int): The number of bathrooms of the place.
+        max_guest (int): The maximum number of guests of the place.
+        price_by_night (int): The price by night of the place.
+        latitude (float): The latitude of the place.
+        longitude (float): The longitude of the place.
+        amenity_ids (list): A list of Amenity ids.
     """
-    __tablename__ = "reviews"
-    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-    text = Column(String(1024), nullable=False)
+
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
