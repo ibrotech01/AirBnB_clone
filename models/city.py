4d213f6c34ee932645d6e673fccd622a7e2a478f
ibrotech01 <45831834+ibrotech01@users.noreply.github.com>
2023-07-31 01:34:39 +0100

    city.py

--- models/city.py
+++ models/city.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-"""This is the city class"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
-from models.place import Place
+"""Defines the City class."""
+from models.base_model import BaseModel
 
 
-class City(BaseModel, Base):
-    """This is the class for City
+class City(BaseModel):
+    """Represent a city.
     Attributes:
-        state_id: The state id
-        name: input name
+        state_id (str): The state id.
+        name (str): The name of the city.
     """
-    __tablename__ = "cities"
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-    name = Column(String(128), nullable=False)
-    places = relationship("Place", cascade="all,delete", backref='cities')
+
+    state_id = ""
+    name = ""
