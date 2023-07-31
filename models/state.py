b39c35a606b7f55d7cbfedf404debad2e7263744
ibrotech01 <45831834+ibrotech01@users.noreply.github.com>
2023-07-31 01:33:10 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,28 +1,12 @@
 #!/usr/bin/python3
-"""This is the state class"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from models.city import City
+"""Defines the State class."""
+from models.base_model import BaseModel
 
 
-class State(BaseModel, Base):
-    """This is the class for State
+class State(BaseModel):
+    """Represent a state.
     Attributes:
-        name: input name
+        name (str): The name of the state.
     """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all,delete", backref="state")
 
-    @property
-    def cities(self):
-        """  getter attribute cities that returns the list of
-        City instances with state_id equals to the current State.id """
-        from models import storage
-        allcities = storage.all(City)
-        c = []
-        for k, v in allcities.items():
-            if v.state_id == self.id:
-                c.append(v)
-        return c
+    name = ""
